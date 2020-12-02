7d1cd278fb470dc58989572f56424074f4a39ff4
bqla <979052538@qq.com>
2020-12-03 03:16:26 +0800

    buqle

--- web/vue.config.js
+++ web/vue.config.js
@@ -7,5 +7,16 @@ module.exports={
                     '@ant-design/icons/lib/dist$': path.resolve(__dirname,'./src/tmpicons.js')  // 自建的文件的相对路径
                 }
             }
+    },
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://acquacap.com/acquacap', //对应自己的接口
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api': ''
+                }
+            }
+        }
     }
 }
